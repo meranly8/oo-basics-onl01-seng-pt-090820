09fb836c48c3aea76ddbf5cd2f8efda32d89c93b
Michelle Ranly <meranly8@gmail.com>
2020-09-24 21:49:44 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,13 @@
 class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
   
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def cobble
+    puts "The shoe has been repaired"
+    @condition = "New"
+  end
 end
